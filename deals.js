0b342682539d9b7c329e9ebb375a6a07a160d248
Strucker.eth <aberamacarleton@gmail.com>
2023-02-11 19:42:23 +0300

    hamburger button function

---
+++ deals.js
@@ -0,0 +1,8 @@
+function burgerMenu() {
+    let burger = document.getElementById("bgm");
+    if(burger.style.display === "block") {
+        burger.style.display = "none";
+    } else {
+        burger.style.display = "block";
+    }
+}   
